cc26af4548096878ffdf8cf8e74792a1a21cf8a3
Zaitox <120062704+Zaitox@users.noreply.github.com>
2022-12-10 22:19:37 +0100

    Update 10.py
    day 10, task 1+2

--- 10/10.py
+++ 10/10.py
@@ -1,7 +1,13 @@
 lines = iter([x.split()for x in open("input.txt")])
-cycle, busy, x, add, s1 = 0, 0, 1, 0, 0
+
+cycle, busy, x, add, s1, buf = 0, 0, 1, 0, 0, ''
 check_on_cycle = (20, 60, 100, 140, 180, 220)
-for i in range(1,221):
+
+def get_pixel(pos):
+    if pos in range (x,x+3): return '#' 
+    return '.'
+
+for i in range(240):
     if not busy:
         x += add
         l = next(lines)
@@ -10,5 +16,10 @@ for i in range(1,221):
             busy += 1
         else: add = 0
     else: busy -= 1
-    if i in check_on_cycle: s1 += x * i
+    if i+1 in check_on_cycle: s1 += x * (i+1)
+    imod = i % 40 +1
+    if imod == 1: buf+='\n'
+    buf+=get_pixel(imod)
+
 print(f'task1: {s1}')
+print(f'task2: {buf}')
